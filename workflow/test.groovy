f1445d16ecd9051bc130f1d5b49d3be77e232452
pganshirt <pganshirt@constantcontact.com>
2017-09-13 21:28:45 -0400

    Update test.groovy

--- workflow/test.groovy
+++ workflow/test.groovy
@@ -1,9 +1,6 @@
 def colorStage() {
     node {
-        // This displays colors using the 'xterm' ansi color map.
-        ansiColor('xterm') {
-            // Just some echoes to show the ANSI color.
-            stage "\u001B[31mI'm Red\u001B[0m Now not"
+        echo "This is the end!!"
         }
     }
 }
