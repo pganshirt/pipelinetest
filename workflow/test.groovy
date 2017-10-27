2bc403ed090e575a5232ec1d5007b76c8cfb64ec
pganshirt <pganshirt@constantcontact.com>
2017-10-27 16:15:38 -0400

    Update test.groovy

--- workflow/test.groovy
+++ workflow/test.groovy
@@ -7,4 +7,10 @@ def prepareComposeEnvFileFromTemplate(String path, String uid)
 {
     sh "cp ${path}/template/.env ${path}"
 }
+def launchEcomContainers(List containersList)
+{
+    for (int i = 0;i < containersList.size(); i++){
+        echo "Running muid: ${containersList[i][0]} and tests: ${containerList[i][1]"
+    }
+}
 return this;
